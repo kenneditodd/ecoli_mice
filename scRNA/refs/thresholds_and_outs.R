8cae1801ba7240019fef959f58f1816f494dd3a6
kenneditodd <kenneditodd@gmail.com>
2025-09-09 11:41:11 -0700

    minor updates

--- scRNA/refs/thresholds_and_outs.R
+++ scRNA/refs/thresholds_and_outs.R
@@ -1,16 +1,9 @@
 # variables
-filtering_method <- "thresh2_no_integration"
+filtering_method <- "thresh1_cellbender"
 out <- paste0("../../results/", filtering_method, "/")
 
 # thresholds
-if (filtering_method == "thresh1_integrated") {
-  nCount.min <- 2500
-  nCount.max <- 28000
-  nFeature.min <- 1200
-  complexity.cutoff <- 0.8
-  mt.cutoff <- 5
-  hb.cutoff <- 1
-} else if (filtering_method == "thresh2_no_integration") {
+if (filtering_method == "thresh1_cellbender") {
   nCount.min <- 2500
   nCount.max <- 28000
   nFeature.min <- 1200

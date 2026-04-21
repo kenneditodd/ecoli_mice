ecaa239d08d331983700fb2d859c147a2af8b00b
Kennedi Todd <kenneditodd@MacBook-Pro-4.local>
2026-04-20 22:13:38 -0700

    sc updates

--- bulkRNA/scripts/13_shiny_app/functions/assign_colors.R
+++
@@ -1,8 +0,0 @@
-assign_colors <- function(data, fdrq, lfc) {
-  data %>%
-    mutate(color_adjpval = case_when(
-      adj.P.Val < fdrq & logFC > lfc ~ "up-regulated",
-      adj.P.Val < fdrq & logFC < -lfc ~ "down-regulated",
-      TRUE ~ "not significant"
-    ))
-}
